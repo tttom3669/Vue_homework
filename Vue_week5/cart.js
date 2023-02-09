77635f303ad667896ec5c15df26a49d11896e5ec
tttom3669 <tttom3669@gmail.com>
2023-02-09 15:59:37 +0800

    git init

---
+++ Vue_week5/cart.js
@@ -0,0 +1,12 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.min.js';
+const apiUrl = "https://vue3-course-api.hexschool.io/v2";
+const apiPath = 'tttom3669';
+
+const app = createApp({
+    data() {
+        return {
+        }
+    },
+});
+
+app.mount('#app');
